ffb28818048aac286a03ca9beb7486293b3f7f9c
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2020-08-07 22:24:34 -0300

    att aula 4 parte 2

--- src/server.js
+++ src/server.js
@@ -1,18 +1,61 @@
+const proffys = [
+    { 
+    name: "Diego Fernandes", 
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whatsapp: "995923654",
+    bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Química",
+    cost: "20",
+    weekday: [0],
+    time_from: [],
+    time_to: []
+    }
+]
+
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
 const express = require('express')
 const server = express()
+const nunjucks = require('nunjucks')
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
 
 server.use(express.static("public"))
 
 server.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 })
 
 server.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
+    return res.render("study.html", { proffys })
 })
 
 server.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+    return res.render("give-classes.html")
 })
 
 server.listen(3000)
